b30f484d86190c24370179c7f36b2eb5db06690e
James Graca-Jones <jamesgracajones@gmail.com>
2023-03-21 21:56:37 +0000

    Initialise express app

---
+++ blog-api/app.js
@@ -0,0 +1,10 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
