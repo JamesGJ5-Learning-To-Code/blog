2473dc686e1d81bf6c18307dc52508bb4c77083c
James Graca-Jones <jamesgracajones@gmail.com>
2023-03-22 20:10:34 +0000

    Move solved notes & TODO

--- blog-api/routes/commentRouter.js
+++ blog-api/routes/commentRouter.js
@@ -1,6 +1,3 @@
-// NOTE: if there are no routes in the router defined below, then in ../postRouter, if the 
-// path supplied is "comments", this may be treated as a postid
-
 const express = require("express");
 const commentControllers = require("../controllers/commentControllers");
 
--- blog-api/routes/postRouter.js
+++ blog-api/routes/postRouter.js
@@ -4,11 +4,6 @@ const postControllers = require("../controllers/postControllers");
 
 const router = express.Router();
 
-// NOTE: ensure that there are routes in commentRouter, otherwise a supplied path of 
-// "comments" will be treated as a postid below
-// TODO: make sure it's safe enough to use commentRouter as it is being used below, given 
-// the postid confusion possibility
-
 router.get("/", postControllers.getPosts);
 router.get("/:postid", postControllers.getPost);
 router.post("/", postControllers.postPost);
