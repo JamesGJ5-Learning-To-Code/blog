71d1cd7da5844c43c50bb1353c8a0db97ad15e99
James Graca-Jones <jamesgracajones@gmail.com>
2023-03-22 19:49:30 +0000

    Add dummy routes to comment router

---
+++ blog-api/controllers/commentControllers.js
@@ -0,0 +1,15 @@
+exports.getComments = (req, res, next) => {
+    res.send("TODO: implement getComments")
+};
+
+exports.postComment = (req, res, next) => {
+    res.send("TODO: implement postComment");
+};
+
+exports.putComment = (req, res, next) => {
+    res.send("TODO: implement putComment");
+};
+
+exports.deleteComment = (req, res, next) => {
+    res.send("TODO: implement deleteComment");
+};
--- blog-api/routes/commentRouter.js
+++ blog-api/routes/commentRouter.js
@@ -2,7 +2,13 @@
 // path supplied is "comments", this may be treated as a postid
 
 const express = require("express");
+const commentControllers = require("../controllers/commentControllers");
 
 const router = express.Router();
+
+router.get("/", commentControllers.getComments);
+router.post("/", commentControllers.postComment);
+router.put("/:postid", commentControllers.putComment);
+router.delete("/:postid", commentControllers.deleteComment);
 
 module.exports = router;
