1910398c0fd6c34862ea3a18efb8ed5a17c60251
James Graca-Jones <jamesgracajones@gmail.com>
2023-03-22 18:05:49 +0000

    Remove user route

--- blog-api/routes/apiRouter.js
+++ blog-api/routes/apiRouter.js
@@ -1,14 +1,12 @@
 const express = require("express");
 require("../controllers/passport");
 const authRouter = require("./authRouter");
-const userRouter = require("./userRouter");
 const postRouter = require("./postRouter");
 const commentRouter = require("./commentRouter");
 
 const router = express.Router();
 
 router.use("/auth", authRouter);
-router.use("/users", userRouter);
 router.use("/posts", postRouter);
 router.use("/comments", commentRouter);
 
