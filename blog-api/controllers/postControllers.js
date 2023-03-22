4ca2c6bfde13973e26a71a0f84e51256b40f51cd
James Graca-Jones <jamesgracajones@gmail.com>
2023-03-22 18:20:54 +0000

    Add dummy routes to post router

---
+++ blog-api/controllers/postControllers.js
@@ -0,0 +1,19 @@
+exports.getPosts = (req, res, next) => {
+    res.send("TODO: implement getPosts");
+};
+
+exports.getPost = (req, res, next) => {
+    res.send("TODO: implement getPost");
+};
+
+exports.postPost = (req, res, next) => {
+    res.send("TODO: implement postPost");
+};
+
+exports.putPost = (req, res, next) => {
+    res.send("TODO: implement putPost");
+};
+
+exports.deletePost = (req, res, next) => {
+    res.send("TODO: implement deletePost");
+};
--- blog-api/routes/postRouter.js
+++ blog-api/routes/postRouter.js
@@ -1,5 +1,13 @@
 const express = require("express");
+const postControllers = require("../controllers/postControllers");
 
 const router = express.Router();
+
+router.get("/", postControllers.getPosts);
+router.get("/:postid", postControllers.getPost);
+router.post("/", postControllers.postPost);
+router.put("/:postid", postControllers.putPost);
+router.delete("/:postid", postControllers.deletePost);
+// TODO: consider adding more routes
 
 module.exports = router;
